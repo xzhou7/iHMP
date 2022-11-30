875aad9d68211a8c98496dec8c35702ff8e8f33a
Xin Zhou <34285739+xzhou7@users.noreply.github.com>
2022-11-30 11:38:21 +0800

    Add files via upload

---
+++ intra_microbiome_correlation/stool_microbiome.R
@@ -0,0 +1,113 @@
+#' ---
+#' title: "Stool microbiome metabolome correlation"
+#' author: 
+#'   - name: "Xiaotao Shen" 
+#'     url: https://www.shenxt.info/
+#'     affiliation: Stanford School of Medicine
+#' date: "`r Sys.Date()`"
+#' site: distill::distill_website
+#' output: 
+#'   distill::distill_article:
+#'     code_folding: false
+#' ---
+
+#+ r setup, echo=TRUE, eval = TRUE, include = TRUE
+
+no_function()
+# set work directory
+
+masstools::setwd_project()
+library(tidyverse)
+rm(list = ls())
+
+source("code/tools.R")
+
+####load data
+# ###load diversity
+# load("data/from_xin/Diversity_Datatable.RData")
+# 
+# ##prevalence
+# load("data/from_xin/Prevalance.RData")
+
+###stool microbiome
+{
+  load("data_analysis/stool_microbiome/data_preparation/expression_data")
+  load("data_analysis/stool_microbiome/data_preparation/sample_info")
+  load("data_analysis/stool_microbiome/data_preparation/variable_info")  
+}
+
+stool_microbiome_expression_data = expression_data
+stool_microbiome_sample_info = sample_info
+stool_microbiome_variable_info = variable_info
+
+###read genus table
+expression_data =
+readr::read_csv(here::here("data/from_xin/Genus Table/ST/Genus_ST.csv")) %>%
+  tibble::column_to_rownames(var = "SampleID") %>%
+  dplyr::select(-c(X1:batch)) %>%
+  t() %>%
+  as.data.frame()
+
+stool_microbiome_variable_info =
+  data.frame(variable_id = rownames(expression_data))
+
+stool_microbiome_expression_data =
+  expression_data
+
+dim(stool_microbiome_sample_info)
+length(unique(stool_microbiome_sample_info$subject_id))
+dim(stool_microbiome_variable_info)
+
+######work directory
+masstools::setwd_project()
+setwd("data_analysis/intra_microbiome_correlation/stool_microbiome")
+
+###only remain the subjects with at least >= 5
+remian_subject_id = 
+stool_microbiome_sample_info %>% 
+  dplyr::group_by(subject_id) %>% 
+  dplyr::summarise(n = n()) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::filter(n > 5) %>% 
+  dplyr::pull(subject_id)
+
+stool_microbiome_sample_info = 
+  stool_microbiome_sample_info %>% 
+  dplyr::filter(subject_id %in% remian_subject_id)
+
+stool_microbiome_expression_data = 
+  stool_microbiome_expression_data[,stool_microbiome_sample_info$sample_id]
+
+##only remain the genus at least in 10% subjects
+remain_idx =
+  which(rowSums(stool_microbiome_expression_data) > 0)
+
+stool_microbiome_expression_data = stool_microbiome_expression_data[remain_idx,]
+stool_microbiome_variable_info = stool_microbiome_variable_info[remain_idx,,drop = FALSE]
+
+remain_idx =
+  stool_microbiome_expression_data %>%
+  apply(1, function(x){
+    sum(as.numeric(x) == 0) / ncol(stool_microbiome_expression_data)
+  }) %>%
+  `<`(0.9) %>%
+  which()
+
+length(remain_idx)
+
+stool_microbiome_expression_data = stool_microbiome_expression_data[remain_idx,]
+stool_microbiome_variable_info = stool_microbiome_variable_info[remain_idx,,drop = FALSE]
+
+##save data
+{
+  save(stool_microbiome_expression_data, file = "stool_microbiome_expression_data")
+  save(stool_microbiome_variable_info, file = "stool_microbiome_variable_info")
+  save(stool_microbiome_sample_info, file = "stool_microbiome_sample_info")
+}
+
+{
+  load("stool_microbiome_expression_data")
+  load("stool_microbiome_variable_info")
+  load("stool_microbiome_sample_info")
+}
+
---
+++ stool_microbiome_metabolome_phenotype/st_prevalence_metabolome_phenotype/st_microbiome_phenotype.R
@@ -0,0 +1,110 @@
+##
+no_function()
+
+masstools::setwd_project()
+library(tidyverse)
+rm(list = ls())
+
+####load data
+###load diversity
+load("data/from_xin/Diversity_Datatable.RData")
+
+##prevalence
+load("data/from_xin/Prevalance.RData")
+st_microbiome_expression_data = ST.Pr %>% 
+  t() %>% 
+  as.data.frame()
+
+###stool microbiome
+load("data_analysis/st_microbiome/data_preparation/sample_info")
+load("data_analysis/st_microbiome/data_preparation/variable_info")
+
+st_microbiome_sample_info = 
+  sample_info %>% 
+  dplyr::distinct(subject_id, .keep_all = TRUE) %>% 
+  dplyr::filter(subject_id %in% colnames(st_microbiome_expression_data))
+
+st_microbiome_expression_data = 
+  st_microbiome_expression_data[,st_microbiome_sample_info$subject_id]
+
+st_microbiome_variable_info = 
+  data.frame(variable_id = rownames(st_microbiome_expression_data))
+
+st_microbiome_sample_info$SSPG = as.numeric(st_microbiome_sample_info$SSPG)
+
+##only remain the genus at least in 1% subjects
+remain_idx = 
+  which(rowSums(st_microbiome_expression_data) > 0)
+
+st_microbiome_expression_data = st_microbiome_expression_data[remain_idx,]
+st_microbiome_variable_info = st_microbiome_variable_info[remain_idx,,drop = FALSE]
+
+
+st_microbiome_expression_data %>% 
+  t() %>% 
+  as.data.frame() %>% 
+  purrr::map(function(x){
+    temp_data =
+      data.frame(x = x,
+                 st_microbiome_sample_info) %>%
+      dplyr::filter(!is.na(SSPG)) %>%
+      dplyr::mutate(SSPG = as.numeric(SSPG),
+                    x = as.numeric(x))
+    
+    temp_data$Gender[temp_data$Gender == 'F'] = 0
+    temp_data$Gender[temp_data$Gender == 'M'] = 1
+    temp_data$Gender = as.numeric(temp_data$Gender)
+    
+    temp_data$Ethnicity[temp_data$Ethnicity == 'C'] = 0
+    temp_data$Ethnicity[temp_data$Ethnicity == 'H'] = 1
+    temp_data$Ethnicity[temp_data$Ethnicity == 'B'] = 2
+    temp_data$Ethnicity[temp_data$Ethnicity == 'A'] = 3
+    temp_data$Ethnicity = as.numeric(temp_data$Ethnicity)
+    
+    glm_reg =
+      glm(SSPG ~ x + Gender + Adj.age + Ethnicity,
+          family = gaussian,
+          temp_data)
+    
+    temp =
+      summary(glm_reg)$coefficients %>%
+      as.data.frame()
+    
+    c(estimate = temp$Estimate[2],
+      glm_p = temp$`Pr(>|t|)`[2])
+    
+    library(ppcor)
+    
+    cor_value =
+      pcor.test(
+        x = temp_data$x,
+        y = temp_data$SSPG,
+        z = temp_data[, c("Gender", "Adj.age", "Ethnicity")],
+        method = "spearman"
+      )
+    
+    result =
+      c(
+        estimate = unname(temp$Estimate[2]),
+        glm_p = unname(temp$`Pr(>|t|)`[2]),
+        cor = unname(cor_value$estimate),
+        cor_p = unname(cor_value$p.value)
+      )
+    
+    if (is.na(result[3])) {
+      result[3] = 0
+    }
+    
+    if (is.na(result[4])) {
+      result[4] = 1
+    }
+    
+    result
+  })
+
+
+
+
+
+
+
