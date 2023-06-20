20a95a2083d3a32d7e637535f95fcede68325a82
Xin Zhou <34285739+xzhou7@users.noreply.github.com>
2023-06-19 22:26:23 -0700

    Add files via upload

---
+++ code/correlation_network/whole_dataset_richness/skin_microbiome/skin_microbiome_vs_proteome_correlation.R
@@ -0,0 +1,355 @@
+#' ---
+#' title: "Skin microbiome proteome correlation"
+#' author: 
+#'   - name: "Xiaotao Shen" 
+#'     url: https://www.shenxt.info/
+#'     affiliation: Stanford School of Medicine
+#' date: "`r Sys.Date()`"
+#' site: distill::distill_website
+#' output: 
+#'   distill::distill_article:
+#'     code_folding: false
+#' ---
+
+#+ r setup, echo=TRUE, eval = TRUE, include = TRUE
+
+no_function()
+# set work directory
+setwd(masstools::get_project_wd())
+library(tidyverse)
+rm(list = ls())
+
+source("code/tools.R")
+
+######work directory
+setwd(masstools::get_project_wd())
+load("data/from_xin/Richness_Bygenus.RData")
+load("data_analysis/skin_microbiome/data_preparation/sample_info")
+setwd("data_analysis/correlation_network/whole_data_set_enrichness/skin_microbiome_vs_proteome")
+
+richness_SK_byGenus
+
+skin_microbiome_expression_data =
+  t(richness_SK_byGenus) %>%
+  as.data.frame()
+
+colnames(skin_microbiome_expression_data) = 
+  sample_info$sample_id[match(colnames(skin_microbiome_expression_data), sample_info$KitID)]
+
+skin_microbiome_sample_info = 
+  sample_info[match(colnames(skin_microbiome_expression_data), sample_info$sample_id),]
+
+colnames(skin_microbiome_expression_data) == skin_microbiome_sample_info$sample_id
+
+skin_microbiome_variable_info =
+  data.frame(
+    variable_id = rownames(skin_microbiome_expression_data),
+    Genus = rownames(skin_microbiome_expression_data)
+  )
+
+ ###plasma proteome
+{
+  load(here::here(
+    "data_analysis/proteome/data_preparation/expression_data"
+  ))
+  load(here::here("data_analysis/proteome/data_preparation/sample_info"))
+  load(here::here(
+    "data_analysis/proteome/data_preparation/variable_info"
+  ))
+}
+
+proteome_expression_data = expression_data
+proteome_sample_info = sample_info
+proteome_variable_info = variable_info
+
+proteome_sample_info$CollectionDate =
+  as.Date(proteome_sample_info$CollectionDate, "%m/%d/%y")
+
+dim(proteome_expression_data)
+length(unique(proteome_sample_info$subject_id))
+
+###match samples
+dim(skin_microbiome_sample_info)
+dim(proteome_sample_info)
+
+length(skin_microbiome_sample_info$subject_id)
+length(unique(skin_microbiome_sample_info$subject_id))
+
+###just matched samples according to sample id, only 1 missed
+intersect_sample_id =
+  intersect(skin_microbiome_sample_info$sample_id,
+            proteome_sample_info$sample_id)
+
+length(intersect_sample_id)
+
+skin_microbiome_expression_data =
+  skin_microbiome_expression_data[,intersect_sample_id]
+
+proteome_expression_data =
+  proteome_expression_data[,intersect_sample_id]
+
+skin_microbiome_sample_info =
+  skin_microbiome_sample_info[match(intersect_sample_id, skin_microbiome_sample_info$sample_id),]
+
+proteome_sample_info =
+  proteome_sample_info[match(intersect_sample_id, proteome_sample_info$sample_id),]
+
+length(unique(proteome_sample_info$subject_id))
+
+###only remain the subjects with at least >= 5
+remian_subject_id =
+  skin_microbiome_sample_info %>%
+  dplyr::group_by(subject_id) %>%
+  dplyr::summarise(n = n()) %>%
+  dplyr::ungroup() %>%
+  dplyr::filter(n > 5) %>%
+  dplyr::pull(subject_id)
+
+proteome_sample_info =
+  proteome_sample_info %>%
+  dplyr::filter(subject_id %in% remian_subject_id)
+
+proteome_expression_data =
+  proteome_expression_data[, proteome_sample_info$sample_id]
+
+skin_microbiome_sample_info =
+  skin_microbiome_sample_info %>%
+  dplyr::filter(subject_id %in% remian_subject_id)
+
+skin_microbiome_expression_data =
+  skin_microbiome_expression_data[,skin_microbiome_sample_info$sample_id]
+
+##only remain the genus at least in 10% subjects
+remain_idx =
+  which(rowSums(skin_microbiome_expression_data) > 0)
+
+skin_microbiome_expression_data = skin_microbiome_expression_data[remain_idx,]
+skin_microbiome_variable_info = skin_microbiome_variable_info[remain_idx,,drop = FALSE]
+
+remain_idx =
+  skin_microbiome_expression_data %>%
+  apply(1, function(x){
+    sum(as.numeric(x) == 0) / ncol(skin_microbiome_expression_data)
+  }) %>%
+  `<`(0.9) %>%
+  which()
+
+length(remain_idx)
+
+skin_microbiome_expression_data = skin_microbiome_expression_data[remain_idx,]
+skin_microbiome_variable_info = skin_microbiome_variable_info[remain_idx,,drop = FALSE]
+
+##save data
+{
+  save(skin_microbiome_expression_data, file = "skin_microbiome_expression_data")
+  save(skin_microbiome_variable_info, file = "skin_microbiome_variable_info")
+  save(skin_microbiome_sample_info, file = "skin_microbiome_sample_info")
+
+  save(proteome_expression_data, file = "proteome_expression_data")
+  save(proteome_variable_info, file = "proteome_variable_info")
+  save(proteome_sample_info, file = "proteome_sample_info")
+}
+
+{
+  load("skin_microbiome_expression_data")
+  load("skin_microbiome_variable_info")
+  load("skin_microbiome_sample_info")
+  
+  load("proteome_expression_data")
+  load("proteome_variable_info")
+  load("proteome_sample_info")  
+}
+
+dim(skin_microbiome_expression_data)
+dim(proteome_expression_data)
+
+###finally, for skin microbiome, 100 genus, for proteome, 302 proteins
+###and 554 samples
+
+######--------------------------------------------------------------------------
+##for raw data, we just log(x+1, 2)
+library(plyr)
+
+#####
+dim(proteome_expression_data)
+dim(skin_microbiome_expression_data)
+
+skin_microbiome_sample_info$subject_id == proteome_sample_info$subject_id
+
+###step 1
+###linear mixed model to adjust the subject ID random effect, and then use the partial correlation
+###to get the correlation between microbiome and proteome
+library(lme4)
+library(rmcorr)
+
+library(future)
+library(furrr)
+
+# skin_microbiome_proteome_lm_adjusted_cor =
+#   lm_adjusted_cor(
+#     data_set1 = skin_microbiome_expression_data,
+#     data_set2 = proteome_expression_data,
+#     sample_info = skin_microbiome_sample_info,
+#     method = "all",
+#     threads = 8
+#   )
+# 
+# skin_microbiome_proteome_lm_adjusted_cor_spearman = skin_microbiome_proteome_lm_adjusted_cor[[1]]
+# 
+# save(
+#   skin_microbiome_proteome_lm_adjusted_cor_spearman,
+#   file = "skin_microbiome_proteome_lm_adjusted_cor_spearman",
+#   compress = "xz"
+# )
+
+load("skin_microbiome_proteome_lm_adjusted_cor_spearman")
+
+###here we use the lm_adjusted_cor
+sum(skin_microbiome_proteome_lm_adjusted_cor_spearman$p_adjust < 0.2)
+
+cor_data =
+  skin_microbiome_proteome_lm_adjusted_cor_spearman %>%
+  dplyr::filter(p_adjust < 0.2)
+
+###output plot
+#####network to show the correlation between microbiome and metabolite
+dim(cor_data)
+
+library(ggraph)
+library(igraph)
+library(tidygraph)
+
+###example network
+library(plyr)
+edge_data =
+  cor_data %>%
+  dplyr::mutate(p = -log(p_adjust, 10)) %>%
+  dplyr::select(microbiome, metabolite, p, p_adjust, cor) %>%
+  dplyr::rename(from = microbiome,
+                to = metabolite,
+                cor = cor) 
+  
+node_data =
+  data.frame(node = unique(c(edge_data$from, edge_data$to))) %>%
+  dplyr::left_join(proteome_variable_info[, c("variable_id", "variable_id")] %>% 
+                     dplyr::rename(protein_name = variable_id.1),
+                   by = c("node" = "variable_id")) %>%
+  dplyr::rename(true_name = protein_name) %>%
+  dplyr::mutate(class = case_when(
+    is.na(true_name) ~ "Skin microbiome",
+    !is.na(true_name) ~ "Proteome"
+  )) %>%
+  dplyr::left_join(skin_microbiome_variable_info[, c("variable_id", "Genus")],
+                   by = c("node" = "variable_id")) %>%
+  dplyr::mutate(true_name =
+                  case_when(is.na(true_name) ~ Genus,
+                            !is.na(true_name) ~ true_name)) %>%
+  dplyr::select(-Genus)
+
+
+node_data =
+  node_data %>%
+  dplyr::filter(!stringr::str_detect(true_name, "C[0-9]{1,2}H[0-9]{1,2}"))
+
+node_data$true_name = stringr::str_replace_all(node_data$true_name, '\"', "")
+
+edge_data =
+  edge_data %>%
+  dplyr::filter(from %in% node_data$node & to %in% node_data$node)
+
+node_data =
+  node_data %>%
+  dplyr::filter(node %in% edge_data$from | node %in% edge_data$to)
+
+####output data
+edge_data$from_true_name = 
+  node_data$true_name[match(edge_data$from, node_data$node)]
+
+edge_data$to_true_name = 
+  node_data$true_name[match(edge_data$to, node_data$node)]
+
+edge_data = 
+  edge_data %>% 
+  dplyr::mutate(significance = case_when(
+    p_adjust < 0.05 ~ "p.adj<0.05",
+    p_adjust >= 0.05 ~ "0.05<p.adj<0.2",
+  ))
+
+library(openxlsx)
+wb <- createWorkbook()
+modifyBaseFont(wb, fontSize = 12, fontName = "Time New Roma")
+addWorksheet(wb, sheetName = "Node data", gridLines = TRUE)
+addWorksheet(wb, sheetName = "Edge data", gridLines = TRUE)
+freezePane(wb, sheet = 1, firstRow = TRUE, firstCol = TRUE)
+freezePane(wb, sheet = 2, firstRow = TRUE, firstCol = TRUE)
+writeDataTable(wb, sheet = 1, x = node_data,
+               colNames = TRUE, rowNames = FALSE)
+writeDataTable(wb, sheet = 2, x = edge_data,
+               colNames = TRUE, rowNames = FALSE)
+saveWorkbook(wb = wb, file = "skin_microbiome_vs_proteome.xlsx", overwrite = TRUE)
+
+temp_data <- 
+  tidygraph::tbl_graph(nodes = node_data, 
+                       edges = edge_data,
+                       directed = FALSE) %>% 
+  dplyr::mutate(Degree = centrality_degree(mode = 'all'))
+
+angle <- 360 * (c(1:nrow(node_data)) - 0.5)/nrow(node_data)
+hjust <- ifelse(angle > 180, 1, 0)
+angle <- ifelse(angle > 180, 90 - angle + 180, 90 - angle)
+
+plot <-
+  ggraph(temp_data,
+         layout = 'fr',
+         circular = FALSE) +
+  geom_edge_link(
+    strength = 1,
+    aes(
+      color = cor,
+      width = abs(cor),
+      linetype = significance
+    ),
+    alpha = 1,
+    show.legend = TRUE
+  ) +
+  scale_edge_linetype_manual(values = c("p.adj<0.05" = 1,
+                                        "0.05<p.adj<0.2" = 2))+
+  geom_node_point(aes(size = Degree,
+                      color = class), 
+                  shape = 16, 
+                  alpha = 1, show.legend = TRUE) +
+  geom_node_text(aes(x = x ,
+                     y = y ,
+                     color = "black",
+                     label = true_name),
+                 repel = TRUE,
+                 size = 2) +
+  # shadowtext::geom_shadowtext(aes(x = x, y = y, 
+  #                                 # label = ifelse(class == "Proteome", NA, true_name),
+  #                                 label = true_name,
+  #                                 color = class), bg.colour = "white") +
+  guides(edge_color = guide_edge_colorbar(title = "Correlation"),
+         color = guide_legend(title = "Class")) +
+  scale_edge_width_continuous(range = c(0.1,2)) +
+  scale_size_continuous(range = c(5, 10)) +
+  scale_color_manual(values = omics_color[c("Proteome", "Skin microbiome")]) +
+  scale_edge_color_gradient2(low = viridis::cividis(n = 2)[1], 
+                             mid = "white", 
+                             high = "red") +
+  ggraph::theme_graph() +
+  theme(plot.background = element_rect(fill = "transparent", color = NA), 
+        panel.background = element_rect(fill = "transparent", color = NA),
+        legend.position = "right",
+        legend.background = element_rect(fill = "transparent", color = NA))
+
+plot
+
+extrafont::loadfonts()
+
+ggsave(plot,
+       filename = "skin_microbiome_proteome_cor_plot_example.pdf",
+       width = 8.5,
+       height = 7)
+
+
+
