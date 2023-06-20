49f79ff2a2cc2c614df605ebb2f548c0c8aa7dab
Xin Zhou <34285739+xzhou7@users.noreply.github.com>
2023-06-19 22:25:42 -0700

    Add files via upload

---
+++ code/correlation_network/IR_IS_comparison/stool_vs_metabolome.R
@@ -0,0 +1,131 @@
+#' ---
+#' title: "Stool microbiome metabolome correlation"
+#' author:
+#'   - name: "Xiaotao Shen"
+#'     url: https://www.shenxt.info/
+#'     affiliation: Stanford School of Medicine
+#' date: "`r Sys.Date()`"
+#' site: distill::distill_website
+#' output:
+#'   distill::distill_article:
+#'     code_folding: false
+#' ---
+
+#+ r setup, echo=TRUE, eval = TRUE, include = TRUE
+
+no_function()
+# set work directory
+
+setwd(masstools::get_project_wd())
+library(tidyverse)
+library(ggraph)
+library(igraph)
+library(tidygraph)
+
+###example network
+library(plyr)
+rm(list = ls())
+
+source("code/tools.R")
+
+######work directory
+setwd(masstools::get_project_wd())
+
+data_IR <-
+  purrr::map(1:20, function(i) {
+    load(
+      file.path(
+        "data_analysis/correlation_network/whole_data_set_IR/stool_microbiome_vs_metabolome_IR_permutation/",
+        paste("edge_data", i, sep = "_")
+      )
+    )
+    load(
+      file.path(
+        "data_analysis/correlation_network/whole_data_set_IR/stool_microbiome_vs_metabolome_IR_permutation/",
+        paste("node_data", i, sep = "_")
+      )
+    )
+    data.frame(
+      node_number = nrow(node_data),
+      edge_number = nrow(edge_data),
+      class = "IR"
+    )
+    
+  }) %>%
+  do.call(rbind, .) %>%
+  as.data.frame()
+
+data_IS <-
+  purrr::map(1:20, function(i) {
+    load(
+      file.path(
+        "data_analysis/correlation_network/whole_data_set_IS/stool_microbiome_vs_metabolome_IS_permutation/",
+        paste("edge_data", i, sep = "_")
+      )
+    )
+    load(
+      file.path(
+        "data_analysis/correlation_network/whole_data_set_IS/stool_microbiome_vs_metabolome_IS_permutation/",
+        paste("node_data", i, sep = "_")
+      )
+    )
+    data.frame(
+      node_number = nrow(node_data),
+      edge_number = nrow(edge_data),
+      class = "IS"
+    )
+    
+  }) %>%
+  do.call(rbind, .) %>%
+  as.data.frame()
+
+dir.create("data_analysis/correlation_network/IR_vs_IS/stool_vs_metabolome",
+           recursive = TRUE)
+setwd("data_analysis/correlation_network/IR_vs_IS/stool_vs_metabolome")
+
+temp_data <-
+  rbind(data_IR,
+        data_IS)
+
+library(ggstatsplot)
+
+plot1 <-
+  ggbetweenstats(
+    data  = temp_data,
+    x     = class,
+    y     = node_number,
+    title = "Node number",
+    p.adjust.method = "fdr",
+    test.type =
+  ) +
+  ggplot2::scale_fill_manual(values = iris_color) +
+  ggplot2::scale_color_manual(values = iris_color) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  labs(x = "", y = "Node number")
+
+plot1
+
+plot2 <-
+  ggbetweenstats(
+    data  = temp_data,
+    x     = class,
+    y     = edge_number,
+    title = "Edge number",
+    p.adjust.method = "fdr",
+    test.type =
+  ) +
+  ggplot2::scale_fill_manual(values = iris_color) +
+  ggplot2::scale_color_manual(values = iris_color) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  labs(x = "", y = "Edge number")
+
+plot2
+
+library(patchwork)
+
+plot <- 
+plot1 + plot2
+
+ggsave(plot, filename = "stool_metabolome.pdf", width = 14, height = 7)
